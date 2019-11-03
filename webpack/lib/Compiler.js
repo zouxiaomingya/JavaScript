b80a6f2f5cd218ca3ae62dfaec52a2833c64c6a7
zouxiaomingya <13870939865@163.com>
2019-11-03 12:31:01 +0800

    feat:add babylon

--- webpack/lib/Compiler.js
+++ webpack/lib/Compiler.js
@@ -1,5 +1,13 @@
 const path = require('path');
 const fs = require('fs');
+const babylon = require('babylon');
+const t = require('@babel/types');
+const traverse = require('@babel/traverse').default;
+const generator = require('@babel/generator').default;
+// babylon 包，主要就是把源码，转换成 ast 语法
+// @babel/traverse
+// @babel/types
+// @babel/generator
 class Compiler {
     constructor(config) {
         this.config = config;
@@ -12,10 +20,26 @@ class Compiler {
         const content = fs.readFileSync(modulePath, 'utf8')
         return content;
     }
-    parse() {
+    parse(source, parentPath) {
+        const ast = babylon.parse(source);
+        traverse(ast, {
+            CallExpression(p) {
+                const node = p.node;
+                if (node.callee.name === 'require') {
+                    node.callee.name === '___webpack_require__';
+                    const moduleName = node.arguments[0].value;
+                    moduleName = moduleName + (path.extname(moduleName) ? '' : '.js');
+                    moduleName = './' + path.join(parentPath, moduleName);
+                    dependencies.push(moduleName);
+                    node.arguments = [t.stringLiteral(moduleName)];
+                }
 
+            }
+        });
+        const sourceCode = generator(ast).code;
+        return { sourceCode, dependencies }
     }
-    buildModule(modulePath) {
+    buildModule(modulePath, isEntry) {
         const source = this.getSource(modulePath);
         const moduleName = './' + path.relative(this.root, modulePath)
         const { sourecCode, dependencies } = this.parse(source, path.dirname(moduleName))
