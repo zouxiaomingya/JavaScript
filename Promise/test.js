bed78fd312e9796e060a522da9d67fc921f00c3f
zouxiaomingya <13870939865@163.com>
2019-11-04 08:34:18 +0800

    feat:add README

--- Promise/test.js
+++ Promise/test.js
@@ -1,18 +1,32 @@
 // 类调用类中的方法
 
-class Animal{
-    constructor(color, fn){
-        this.color = color;
-        fn.call(this,this.getColor.bind(this));
-    }
-    getColor (){
-        console.log(this)
-        return this.color;
-    }
-}
+// class Animal{
+//     constructor(color, fn){
+//         this.color = color;
+//         fn.call(this,this.getColor.bind(this));
+//     }
+//     getColor (){
+//         console.log(this)
+//         return this.color;
+//     }
+// }
 
-const cat = new Animal('black', function(fn){
-    console.log(this, '>>>>')
-    const color = fn();
-    console.log(color);
+// const cat = new Animal('black', function(fn){
+//     console.log(this, '>>>>')
+//     const color = fn();
+//     console.log(color);
+// })
+
+function getList() {
+    return new Promise((resolve, reject)=>{
+        resolve(222);
+        return 11
+    })
+}
+const list = getList().then(data=>{
+    console.log(data, '>>>')
+    return 1;
+}).then(data => {
+    console.log(data, '%%%%');
 })
+console.log(list);
