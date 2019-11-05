d6f86fee8e1740b7f6fff57ca1b658d60fc38481
zouxiaomingya <13870939865@163.com>
2019-11-06 01:10:46 +0800

    fix

--- Promise/test.js
+++ Promise/test.js
@@ -1,21 +1,21 @@
 // 类调用类中的方法
 
-class Animal{
-    constructor(color, fn){
-        this.color = color;
-        fn.call(this,this.getColor.bind(this));
-    }
-    getColor (){
-        console.log(this)
-        return this.color;
-    }
-}
+// class Animal{
+//     constructor(color, fn){
+//         this.color = color;
+//         fn.call(this,this.getColor.bind(this));
+//     }
+//     getColor (){
+//         console.log(this)
+//         return this.color;
+//     }
+// }
 
-const cat = new Animal('black', function(fn){
-    console.log(this, '>>>>')
-    const color = fn();
-    console.log(color);
-})
+// const cat = new Animal('black', function(fn){
+//     console.log(this, '>>>>')
+//     const color = fn();
+//     console.log(color);
+// })
 
 function getList() {
     return new Promise((resolve, reject)=>{
