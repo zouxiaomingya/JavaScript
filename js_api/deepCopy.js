926f7f94731eaca266fc9e5dbd6955bbc988817e
zouxiaomingya <13870939865@163.com>
2019-11-20 21:39:55 +0800

    feat: MinCoinChange

--- js_api/deepCopy.js
+++ js_api/deepCopy.js
@@ -59,3 +59,56 @@ function deepCopy(target) {
   }
   return _deepCopy(target);
 }
+
+// function MinCoinChange(coins) {
+//   var coins = coins;
+//   var cache = {};
+//   this.makeChange = function(amount) {
+//     var change = [],
+//       total = 0;
+//     for (var i = coins.length; i >= 0; i--) {
+//       var coin = coins[i];
+//       while (total + coin <= amount) {
+//         change.push(coin);
+//         total += coin;
+//       }
+//     }
+//     return change;
+//   };
+// }
+
+class MinCoinChange {
+  constructor(coins) {
+    this.coins = coins;
+    this.cache = {};
+  }
+
+  makeChange(amount) {
+    if (!amount) return [];
+    if (this.cache[amount]) return this.cache[amount];
+    let min = [],
+      newMin,
+      newAmount;
+    this.coins.forEach(coin => {
+      newAmount = amount - coin;
+      if (newAmount >= 0) {
+        newMin = this.makeChange(newAmount);
+      }
+      if (
+        newAmount >= 0 &&
+        (newMin.length < min.length - 1 || !min.length) &&
+        (newMin.length || !newAmount)
+      ) {
+        min = [coin].concat(newMin);
+      }
+    });
+    return (this.cache[amount] = min);
+  }
+}
+
+const rninCoinChange = new MinCoinChange([1, 5, 10, 25]);
+console.log(rninCoinChange.makeChange(36));
+// [1, 10, 25]
+const minCoinChange2 = new MinCoinChange([1, 3, 4]);
+console.log(minCoinChange2.makeChange(6));
+// [3, 3]
